e6f48e482a84d7c8ef1b215f13a35872fbae2bae
billyprakoso <billy2perakhoso@gmail.com>
2023-10-08 09:51:03 +0700

    'oke'

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+// Nama = Billy Dwi Prakoso
+// Class B Morning Group 9 - Web Development
+
+// if
+let namaku = "Billy Dwi Prakoso";
+
+if (namaku === "Billy Dwi Prakoso") {
+  console.log(`Hai, ${namaku}`);
+}
